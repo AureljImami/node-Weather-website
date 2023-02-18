3d46e463a07f7d76f0fb31fb8f63761ae260ab16
Aurelj <imamiaurelj@gmail.com>
2023-02-18 15:02:27 +0100

    bug fix 1

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,6 @@
 //             console.log(data.forecastData)
 //         }
 //     })
-
 // })
 
 const weatherForm = document.querySelector('form')
@@ -27,7 +26,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
 
         response.json().then((data) => {
             if (data.error) {
