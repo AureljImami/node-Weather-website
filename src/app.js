e168f8799a08f1613ec9e4be0b7b28668532e162
Aurelj <imamiaurelj@gmail.com>
2023-02-18 14:46:52 +0100

    preparing dep. on render

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geoCode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const PORT = process.env.PORT || 3030
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -107,8 +108,8 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
 
-    console.log('Server is up on port 3000')
+    console.log('Server is up on port '+ PORT)
 
 })
